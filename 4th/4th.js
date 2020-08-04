349eaf3d8f0953e0d533215a5883ce91f3c2f69a
Emily <haach111000@gmail.com>
2020-08-05 07:25:01 +0800

    burger選單

---
+++ 4th/4th.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+  $( ".burger-menu" ).click(function() {
+
+    $('.nav-list').toggleClass('active',100000);
+    
+  })
+  
+  
+  });
+ 
+  /*$(document).ready(function(){
+  $( ".burger-menu" ).click(function() {
+
+    $('.nav-list').toggleClass('active');
+  })
+  
+  
+  });*/
